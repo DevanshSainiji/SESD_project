027b548c76abc46c6ae76b126ce7e73235bcf8ed
DevanshSainiji <sainidevansh1308@gmail.com>
2026-04-19 20:37:07 +0530

    [FRONTEND] API Layer: Implement Axios service layer

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000/api';
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  stock: number;
+}
+
+export interface Sale {
+  id: number;
+  date: string;
+  customerId: number;
+  userId: number;
+  totalAmount: number;
+  items: any[];
+}
+
+export const ProductService = {
+  getProducts: () => api.get<Product[]>('/products').then(res => res.data),
+  createProduct: (product: Omit<Product, 'id'>) => api.post<Product>('/products', product).then(res => res.data),
+};
+
+export const SaleService = {
+  getSales: () => api.get<Sale[]>('/sales').then(res => res.data),
+  createSale: (saleData: any) => api.post<Sale>('/sales', saleData).then(res => res.data),
+};
